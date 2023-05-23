135cee3b4a5168f8317c79f72de7132bce2066f1
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2023-05-23 16:35:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const usuario = "ángel20099";
-const pass = "pugsitos10";
+const usuario1 = "ángel20099";
+const pass1 = "pugsitos10";
 let dato1 = document.getElementById("username");
 let dato2 = document.getElementById("password");
 const form = document.getElementById("form");
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    if(dato1.value == usuario && dato2.value == pass){
+    if(dato1.value == usuario1 && dato2.value == pass1){
         alert("La contraseña es correcta");
     }
     else{
